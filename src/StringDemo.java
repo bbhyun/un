257bc3af1de11c081d7a2126f664644cfe44a322
uuuser <1435233930@qq.com>
2017-11-24 16:46:05 +0800

    time

--- src/StringDemo.java
+++ src/StringDemo.java
@@ -1,14 +1,20 @@
+import java.util.Date;
 public class StringDemo {
-    public  static void main(String arge[]) {
+    public static void main(String arge[]) {
        /* char[] helloArray = {'h','e','l','l','o','.'};
         String helloString = new String(helloArray);
         System.out.println(helloString);
     }
 }
 */
-        String palindrome = "Dot saw I was Tod";
+       /* String palindrome = "Dot saw I was Tod";
         int len = palindrome.length();
         System.out.println("String length is :" + len);
     }
     }
-StringBuffer
+StringBuffer*/
+        Date date = new Date();//初始化Date对象
+        System.out.println(date.toString());//显示当前时间
+
+    }
+}
