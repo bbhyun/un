dfed54183666c9e7312603e8db395d3a7d357c1e
uuuser <1435233930@qq.com>
2017-11-24 11:52:06 +0800

    修改

---
+++ src/StringDemo.java
@@ -0,0 +1,13 @@
+public class StringDemo {
+    public  static void main(String arge[]) {
+       /* char[] helloArray = {'h','e','l','l','o','.'};
+        String helloString = new String(helloArray);
+        System.out.println(helloString);
+    }
+}
+*/
+        String palindrome = "Dot saw I was Tod";
+        int len = palindrome.length();
+        System.out.println("String length is :" + len);
+    }
+    }
